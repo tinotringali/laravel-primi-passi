eb726601d7f29a217c73855451686a4cd61ac9ef
tinotringali <tringali.tino94@gmail.com>
2023-05-01 20:19:10 +0200

    primi passi con laravel

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Primi Passi</title>
+</head>
+
+<body>
+    <h1>Ciao classe 89!</h1>
+</body>
+
+</html>
